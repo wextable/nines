62c1ce841d8f950c8f2ac4c8d517525952383702
Wesley St. John <wesley.stjohn@mobileforming.com>
2015-08-12 21:46:10 -0700

    initial commit

---
+++ Nines/Source/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nines
+//
+//  Created by Wesley St. John on 8/12/15.
+//  Copyright (c) 2015 WarEagles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nines/Source/ViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nines
+//
+//  Created by Wesley St. John on 8/12/15.
+//  Copyright (c) 2015 WarEagles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
